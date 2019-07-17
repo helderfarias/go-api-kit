1f16f744e0e2381f575657da409c1757e9f7d6c4
Helder Farias <helder.farias@dataprev.gov.br>
2019-07-17 12:25:36 -0300

    Add middlewares

---
+++ constants/db.go
@@ -0,0 +1,3 @@
+package 
+
+type DatabaseContextValue string
---
+++ endpoint/middleware.go
@@ -0,0 +1,59 @@
+package endpoint
+
+import (
+	"context"
+
+	"github.com/helderfarias/go-api-kit/constants"
+	"github.com/helderfarias/go-api-kit/db"
+	"github.com/sirupsen/logrus"
+)
+
+func Database(dbfactory db.ConnectionFactory, key string) Middleware {
+	return func(next Endpoint) Endpoint {
+		return func(parent context.Context, request interface{}) (response interface{}, err error) {
+			conn := dbfactory.NewConnection()
+			if err != nil {
+				return nil, err
+			}
+
+			ctx := context.WithValue(parent, constants.DatabaseContextValue(key), conn)
+			return next(ctx, request)
+		}
+	}
+}
+
+func DatabaseWithTx(dbfactory db.ConnectionFactory, key string) Middleware {
+	return func(next Endpoint) Endpoint {
+		return func(parent context.Context, request interface{}) (response interface{}, err error) {
+			tx, err := dbfactory.NewConnectionWithTransaction()
+			if err != nil {
+				return nil, err
+			}
+
+			ctx := context.WithValue(parent, constants.DatabaseContextValue(key), tx)
+
+			defer func() {
+				if r := recover(); r != nil {
+					if err := tx.Rollback(); err != nil {
+						logrus.Error(err)
+					} else {
+						logrus.Error(err)
+					}
+				}
+			}()
+
+			resp, err := next(ctx, request)
+			if err == nil {
+				if err := tx.Commit(); err != nil {
+					logrus.Error(err)
+				}
+			} else {
+				if err := tx.Rollback(); err != nil {
+					logrus.Error(err)
+				}
+			}
+
+			return resp, err
+		}
+	}
+}
